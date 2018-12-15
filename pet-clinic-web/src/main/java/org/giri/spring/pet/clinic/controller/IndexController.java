d0d5f6ce29b213b316917a7798f8f022f91a1285
giriprasad1 <giriprasad.m@yahoo.com>
2018-12-15 21:29:06 +0530

    Added index html and controller, close #6

---
+++ pet-clinic-web/src/main/java/org/giri/spring/pet/clinic/controller/IndexController.java
@@ -0,0 +1,14 @@
+package org.giri.spring.pet.clinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping({"","/","/index","/index.html"})
+	public String index() {
+		return "index";
+	}
+
+}
