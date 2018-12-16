ab317dd1ec17970032e73ffb213aa1d112ea6f0c
giriprasad1 <giriprasad.m@yahoo.com>
2018-12-16 06:14:55 +0530

    Added bootstrap, close #11

--- pet-clinic-web/src/main/java/org/giri/spring/pet/clinic/controller/VetsController.java
+++ pet-clinic-web/src/main/java/org/giri/spring/pet/clinic/controller/VetsController.java
@@ -1,13 +1,28 @@
 package org.giri.spring.pet.clinic.controller;
 
+import org.giri.spring.pet.clinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetsController {
 	
+	private final VetService vetsService;
+	
+	
+	
+	public VetsController(VetService vetsService) {
+		super();
+		this.vetsService = vetsService;
+	}
+
+
+
 	@RequestMapping({"/vets","/vets/index","/vets/index.html"})
-	public String listVets() {
+	public String listVets(Model model) {
+		
+		model.addAttribute("vets", this.vetsService.findAll());
 		return "vets/index";
 	}
 	
