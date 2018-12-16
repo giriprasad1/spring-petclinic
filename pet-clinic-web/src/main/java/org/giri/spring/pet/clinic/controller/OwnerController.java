d69a858d87185ab1224eda6ae9e3097876f4a47f
giriprasad1 <giriprasad.m@yahoo.com>
2018-12-16 06:21:31 +0530

    Owner page and controller, close #12

--- pet-clinic-web/src/main/java/org/giri/spring/pet/clinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/org/giri/spring/pet/clinic/controller/OwnerController.java
@@ -1,13 +1,27 @@
 package org.giri.spring.pet.clinic.controller;
 
+import org.giri.spring.pet.clinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OwnerController {
 	
+	private final OwnerService ownerService;
+	
+	
+	
+	public OwnerController(OwnerService ownerService) {
+		super();
+		this.ownerService = ownerService;
+	}
+
+
+
 	@RequestMapping({"/owner","/owner/index","/owner/index.html"})
-	public String listVets() {
+	public String listOwners(Model model) {
+		model.addAttribute("owners", this.ownerService.findAll());
 		return "owner/index";
 	}
 	
